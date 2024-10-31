6932b7ac9312f1fd15e21e178a69f9055d1e3849
Georgekuprashvili <kuprashviligeorge@gmail.com>
2024-10-31 20:32:16 +0400

    1

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 let button1 = document.getElementById("button1");
-let button2 = document.getElementById("button2");
 let list = document.getElementById("list");
 
 button1.addEventListener("click", () => {
   let info = prompt("შეიყვანეთ ინფორმაცია");
-  let li = document.createElement("li");
-  li.innerText = info;
-  list.appendChild(li);
-});
-button2.addEventListener("click", () => {
-  if (list.firstChild) {
-    list.removeChild(list.firstChild);
+  if (info) {
+    let li = document.createElement("li");
+    li.innerText = info;
+    list.appendChild(li);
+
+    let removeButton = document.createElement("button");
+    removeButton.innerText = "წაშლა";
+    removeButton.addEventListener("click", () => {
+      li.remove();
+    });
+    li.appendChild(removeButton);
   }
 });
